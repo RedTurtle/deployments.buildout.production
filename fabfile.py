68e96374fbb717999f6319b2a5844059d68fc36f
Andrew Mleczko <andrew@mleczko.net>
2013-04-29 17:21:47 +0200

    cleanup fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,28 @@
-from fabric.api import lcd, local
-from fabric.utils import warn
+"""
+Commands available to deploy using rt.commands
+"""
+# this is necessary to make the fabric commands available
+from fabric.api import env
+from rt.commands import *
+from rt.commands.project import *
+from rt.commands.buildout_components import *
 
+# General metadata
 
-COMPONENTS = {'plone': 'https://github.com/RedTurtle/deployments.buildout.plone/archive/master.tar.gz'}
+# This is the trac/svn/ name
+# Default: ""
+env.project_name = ""
 
+# This is the buildout repository
+# Either SVN or GIT buildout repository
+env.buildout_repo = ""
 
-def need(component):
-    """clone component to ./components directory"""
-    with lcd('components'):
-        url = COMPONENTS.get(component)
-        if not url:
-            warn('Unknown component [%s]' % component)
-            return
-        local('wget %s -O %s.tar.gz' % (url, component))
-        local('mkdir %s -p' % component)
-        local('tar -xvf %(name)s.tar.gz --strip 1 -C %(name)s' % {'name':component})
-        local('rm *.tar.gz')
+# This is the main python version
+env.python_version = '2.7'
+
+# This is the buildout script to use
+env.buildout_cfg = 'buildout.cfg'
+
+# And some host
+env.staging_host = ''
+env.production_host = ''
